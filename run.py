27e805f07ad4fec9a0379b4a6d6b34be7846034c
arslansherazi <arslanhaider.dev@hotmail.com>
2022-06-21 14:16:11 +0500

    running script

--- run.py
+++ run.py
@@ -2,24 +2,25 @@ import logging
 
 from scrapy.signalmanager import dispatcher
 from scrapy import signals
-from twisted.internet import reactor, defer
+from twisted.internet import reactor
 from scrapy.utils.project import get_project_settings
 from scrapy.crawler import CrawlerRunner
-from scrapy.utils.log import configure_logging
 
 from scraper.spiders.beauty_products import BeautyProductsSpider
 from scraper.spiders.laptops_details import LaptopsDetailsSpider
 
-logger = logging
+logger = logging.getLogger('logs')
 
 
-def run():
-    runner = CrawlerRunner(get_project_settings())
-    configure_logging()
+class RunSpiders(object):
+    def __init__(self):
+        self.runner = CrawlerRunner(get_project_settings())
 
+    @staticmethod
     def crawler_results(signal, sender, item, response, spider):
         logger.info(item)
 
+    @staticmethod
     def spider_opened(spider):
         """
         Triggers and save logs in db when any spider is called
@@ -27,9 +28,10 @@ def run():
         :param spider: spider
         """
         spider_name = spider.name.capitalize()
-        message = f"{spider_name} crawler started successfully"
+        message = f"{spider_name} spider started successfully"
         logger.info(message)
 
+    @staticmethod
     def spider_closed(spider, reason):
         """
         Triggers and save logs in db when any spider is finished its processing
@@ -43,18 +45,22 @@ def run():
             message = f'{spider_name} crawler ended with error(s)'
         logger.info(message)
 
-    dispatcher.connect(spider_opened, signal=signals.spider_opened)
-    dispatcher.connect(spider_closed, signal=signals.spider_closed)
-    dispatcher.connect(crawler_results, signal=signals.item_passed)
-
-    @defer.inlineCallbacks
-    def crawl():
-        yield runner.crawl(BeautyProductsSpider)
-        yield runner.crawl(LaptopsDetailsSpider)
+    def run(self):
+        yield self.runner.crawl(BeautyProductsSpider)
+        yield self.runner.crawl(LaptopsDetailsSpider)
         reactor.stop()
-    crawl()
-    reactor.run()
+
+
+def dispatch_signals():
+    """
+    Dispatches crawler signals
+    """
+    dispatcher.connect(RunSpiders.spider_opened, signal=signals.spider_opened)
+    dispatcher.connect(RunSpiders.spider_closed, signal=signals.spider_closed)
+    dispatcher.connect(RunSpiders.crawler_results, signal=signals.item_passed)
 
 
 if __name__ == '__main__':
-    run()
+    dispatch_signals()
+    RunSpiders().run()
+    reactor.run()
